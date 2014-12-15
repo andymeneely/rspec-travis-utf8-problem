4df819c2f20365cd78870cfd30e21ada013a8131
Andy Meneely <andy@se.rit.edu>
2014-12-15 11:34:24 -0500

    Setting this up

---
+++ spec/charset_spec.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+describe 'string character sets' do 
+
+  it 'works reflexively' do
+    expect('a').to eq('a')
+  end
+
+  it 'works on both escaped' do
+    expect("\u00B5").to eq("\u00B5")
+  end
+
+  it 'works when both inline' do
+    expect("µ").to eq("µ")
+  end
+
+  it 'works when inline and escaped' do
+    expect("♠").to eq("\u2660")
+  end
+
+  it 'works on written file compared with escaped' do
+    File.open("tmp.txt", 'w+') { |f| f.write("\u2660") }
+    str = File.open("tmp.txt", 'r:UTF-8').read
+    expect("\u2660").to eq(str)
+  end
+
+end
